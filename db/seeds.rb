0acf7ee8aceccf3be81f442917f2b6d6de976490
Stijn Smekens <stijnsmekens@gmail.com>
2017-05-24 16:04:56 +0200

    with form in restaurant show-page, rake doesn't work on this

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    phone_number: "053778939",
+    category:     "belgian"
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    phone_number: "054778939",
+    category:     "chinese"
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    phone_number: "052778939",
+    category:     "belgian"
+  },
+  {
+    name:         "Comme chez soi",
+    address:      "route de Suresnes 75016 Paris",
+    phone_number: "053778939",
+    category:     "italian"
+  },
+  {
+    name:         "Frietkot",
+    address:      "route de Suresnes 75016 Paris",
+    phone_number: "059778939",
+    category:     "belgian"
+  }
+]
+Restaurant.create!(restaurants_attributes)
+
+puts 'Finished!'
